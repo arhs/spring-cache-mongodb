b1bcb83c6e4f2ad429bb6826901650cf5ecc05a0
Dan Blaisdell <dan.blaisdell@sprinklr.com>
2017-09-14 16:25:13 -0700

    unit tests for MongoCacheBuilder

--- src/test/java/com/arhs/spring/cache/mongo/MongoCacheBuilderTest.java
+++ src/test/java/com/arhs/spring/cache/mongo/MongoCacheBuilderTest.java
@@ -1,4 +1,47 @@
 package com.arhs.spring.cache.mongo;
 
+import com.arhs.spring.cache.mongo.serializer.JavaSerializer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TestConfiguration.class)
 public class MongoCacheBuilderTest {
+
+	private static final String CACHE_NAME = "cache";
+	private static final String COLLECTION_NAME = "test";
+	private static final long TTL = 0;
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+	@Test
+	public void testBuildWithSerializer() {
+		MongoCacheBuilder builder = new MongoCacheBuilder(
+			COLLECTION_NAME,
+			mongoTemplate,
+			CACHE_NAME
+		);
+		builder.withSerializer(new JavaSerializer());
+		builder.withTTL(TTL);
+		builder.withFlushOnBoot(false);
+		builder.build();
+	}
+
+	@Test
+	public void testBuildWithoutSerializer() {
+		MongoCacheBuilder builder = new MongoCacheBuilder(
+			COLLECTION_NAME,
+			mongoTemplate,
+			CACHE_NAME
+		);
+		builder.withTTL(TTL);
+		builder.withFlushOnBoot(false);
+		builder.build();
+	}
+
 }
